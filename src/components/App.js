429399dd7d5c04357ed96cb57bf525d66e9d0210
Rafael Mora <rafael.mora.guti@gmail.com>
2018-05-16 16:46:13 -0400

    Created file structure and index

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to my starter app</h1>
+      </div>
+    );
+  }
+}
